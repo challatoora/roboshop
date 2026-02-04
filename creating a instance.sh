3c1d2d5466438eb088489593d9e26c4b17a3f933
challatoor murali <challatoor murali.gmail.com>
2026-02-04 21:49:21 +0530

    sucess

---
+++ creating a instance.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+AMI_ID="ami-0220d79f3f480ecf5"
+SECURITY_GROUP_IDS="sg-07a628098640c0e6c"
+INSTANCE_TYPE="t3.micro"
+
+for instance in $@{
+    do
+
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type "$INSTANCE_TYPE" \
+    --security-group-ids "$SECURITY_GROUP_IDS" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=NAME,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+            )
+    else
+         
+         IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+            )
+    fi
+    done
+}
+
+
