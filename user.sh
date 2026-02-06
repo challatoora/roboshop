fa529fe960c8752e27668074834cf9e3766b2462
challatoor murali <challatoor murali.gmail.com>
2026-02-06 22:34:53 +0530

    sucess

---
+++ user.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+userid=$(id -u)
+folder="/var/log/shell-roboshop"
+log_file="$folder/$0.log"
+Place=$PWD
+Mongodb_host=mongodb.mreddy.online
+
+
+if [ $userid -ne 0 ]; then
+    echo -e "You are not a root user" | tee -a $log_file
+    exit 1
+fi
+
+mkdir -p $folder
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is failure" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 is success" | tee -a $log_file
+    fi
+
+
+}
+
+dnf module disable nodejs -y
+
+dnf module enable nodejs:20 -y
+
+# dnf install nodejs -y
+# if [ $? -ne 0 ]; then
+
+#  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+# fi
