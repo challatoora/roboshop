02baaf35691b29c1edc1b1de233c19c5b1992861
challatoor murali <challatoor murali.gmail.com>
2026-02-06 22:18:13 +0530

    sucess

---
+++ redis.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+userid=$(id -u)
+folder="/var/log/shell-roboshop"
+log_file="$folder/$0.log"
+
+
+if [ $userid -ne 0 ]; then
+    echo -e "You are not a root user" | tee -a $log_file
+    exit 1
+fi
+
+mkdir -p $folder
+
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is failure" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 is success" | tee -a $log_file
+    fi
+}
+  dnf module disable redis -y &>>$log_file
+  validate $? " disable the redis"
+
+  dnf module enable redis:7 -y &>>$log_file
+  validate $? " enable the redis 7"
+
+  dnf install redis -y &>>$log_file
+  validate $? " installing redis"
+
+  set -i -e "s/127.0.0.1/0.0.0.0/g" -e "/protected-mode/ c protected-mode no" /etc/redis/redis.conf
+
+  
+  systemctl enable redis 
+  validate $? "enable the redis"
+  
+  systemctl start redis 
+  validate $? "enable the redis"
+
+
+
+
+
