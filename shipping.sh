042768ccc7c00217fdfac325eefce8ec86813025
challatoor murali <challatoor murali.gmail.com>
2026-02-07 12:22:16 +0530

    sucess

--- shipping.sh
+++ shipping.sh
@@ -69,11 +69,11 @@ validate $? "starting shiping"
 
 dnf install mysql -y 
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h mysql.mreddy.online -uroot -pRoboShop@1 < /app/db/schema.sql
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql 
+mysql -h mysql.mreddy.online -uroot -pRoboShop@1 < /app/db/app-user.sql 
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql.mreddy.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl restart shipping
 
