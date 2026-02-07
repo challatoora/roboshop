de7596daa7d3ccedcc2c70daf206e6625a3a922e
challatoor murali <challatoor murali.gmail.com>
2026-02-07 15:14:05 +0530

    sucess

--- redis.sh
+++ redis.sh
@@ -22,8 +22,6 @@ validate(){
 }
 
   dnf module disable redis -y &>>$log_file
-  validate $? " disable the redis"
-
   dnf module enable redis:7 -y &>>$log_file
   validate $? " enable the redis 7"
 
@@ -31,11 +29,9 @@ validate(){
   validate $? " installing redis"
 
   sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
- validate $? "allaowing remote connection"
+  validate $? "allaowing remote connection"
   
   systemctl enable redis &>>$log_file
-  validate $? "enable the redis"
-
   systemctl start redis 
   validate $? "enable the redis"
 
